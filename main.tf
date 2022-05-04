a0c3b227a13c66527206ae1507ae63989144f651
Zach <zach.kleine18@gmail.com>
2022-05-04 16:29:29 -0400

    test resourcegroup

--- main.tf
+++ main.tf
@@ -2,7 +2,21 @@
 terraform {
     required_providers {
         azurerm = {
-
+            source = "hashicorp/azurerm"
+            version = "~>2.31.1"
         }
+    }
+}
+
+provider "azurerm " {
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "test-rg"
+    location = "southcentralus"
+    tags = {
+        environment = "dev"
+        source = "terraform"
     }
 }

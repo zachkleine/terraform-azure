0c551dc24131ef908f1afe87835ba9fb3acb450f
Zach <zach.kleine18@gmail.com>
2022-05-11 10:25:56 -0400

    adding to variables.tf and main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "vnet_main" {
   name     = var.rg_name
   location = var.location
   tags = {
@@ -20,4 +20,22 @@ resource "azurerm_resource_group" "rg" {
     source      = "terraform"
     owner       = "zach"
   }
+}
+
+module "vnet-main" {
+  source = "Azure/vnet/azurerm"
+  resource_group_name = var.rg_name
+  vnet_name = var.rg_name
+  address_space = var.vnet_cidr_range
+  subnet_prefixes = var.subnet_prefix
+  subnet_names = var.subnet_names
+  nsg_ids = {}
+  tags = {
+    environment = "dev"
+    costcenter = "IT"
+  }
+}
+
+output "vnet_id" {
+  value = module.vnet-main.vnet_id
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,18 @@ variable "location" {
     type = string
     default = "eastus"
 }
+
+variable "vnet_cidr_range" {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "subnet_prefix" {
+    type = list(string)
+    default = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "subnet_names" {
+  type = list(string)
+  default = ["web", "database"]
+}
